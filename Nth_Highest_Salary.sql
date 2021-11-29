fde06d8b496ead468dab7a68775815557b12859e
AdinaQ <61895538+AdinaQ@users.noreply.github.com>
2021-11-29 10:37:51 -0500

    Create Nth_Highest_Salary.sql

---
+++ Nth_Highest_Salary.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set N=N-1;
+  RETURN (
+      ifnull((select distinct Salary as getNthHighestSalary from Employee order by Salary desc limit 1 offset N),null)
+  );
+END
